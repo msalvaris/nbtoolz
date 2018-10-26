f182ae72404f309d02919151102f49d7604c7612
MSalvaris <msalvaris@users.noreply.github.com>
2018-10-26 15:29:50 +0100

    Adds setup instructions

---
+++ setup.py
@@ -0,0 +1,58 @@
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+
+# Package meta-data.
+NAME = "nbtoolz"
+DESCRIPTION = "A set of handy utilities for Jupyter notebooks"
+URL = ""
+EMAIL = ""
+AUTHOR = "Mathew Salvaris"
+LICENSE = ""
+
+
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert("README.md", "rst")
+except (IOError, ImportError):
+    long_description = "A set of handy utilities for Jupyter notebooks"
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, "__version__.py")) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name=NAME,
+    version=about["__version__"],
+    url=URL,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    scripts=["nbtoolz"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Data Scientists & Developers",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
